2993134155ae49b2a2df3e07e88054a811605230
Yuexuan Li <yuexuanli@microsoft.com>
2022-03-16 22:21:48 +0800

    Initial Commit

---
+++ FitnessCount/FitnessCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessCountApp.swift
+//  FitnessCount
+//
+//  Created by Yuexuan Li on 2022/3/16.
+//
+
+import SwiftUI
+
+@main
+struct FitnessCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
